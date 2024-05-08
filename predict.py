2c76a6bab745cf01ef162ac7f8016a389d9b5672
BenjaminSchaar <122351773+BenjaminSchaar@users.noreply.github.com>
2024-05-08 13:30:36 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -88,7 +88,7 @@ def main(arg_list=None):
 
     logging.info('Model loaded!')
 
-    reader_obj = MicroscopeDataReader(args.input_filepath, as_raw_tiff=True, raw_tiff_num_slices=1)
+    reader_obj = MicroscopeDataReader(args.input_file_path, as_raw_tiff=True, raw_tiff_num_slices=1)
     tif = da.squeeze(reader_obj.dask_array)
     with tiff.TiffWriter(args.output_filepath, bigtiff=True) as tif_writer:
         for i, img in enumerate(tif):
